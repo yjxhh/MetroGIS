cd7f943847f7585e768f73654f8031894a3d40f7
yjxhh <101241704+yjxhh@users.noreply.github.com>
2026-09-22 10:39:35 +0800

    test: update current API usage

--- tests/test_overpass.py
+++ tests/test_overpass.py
@@ -8,7 +8,13 @@ from metrogis.api.overpass import search_metro_line
 def test_line():
 
     data = search_metro_line(
-        "13"
+        "13",
+        (
+            38.8,
+            116.8,
+            39.4,
+            117.8,
+        )
     )
 
     assert "elements" in data
